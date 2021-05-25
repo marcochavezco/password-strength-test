aea7193f25eb8cca2b8d2ead0de23c770323d769
marcochavezco <machavezco@gmail.com>
2021-05-25 18:45:48 -0500

    First commit

---
+++ src/password_strength_test.py
@@ -0,0 +1,18 @@
+import re
+
+user_password = str(input("Enter a password: "))
+
+# (?=...) positive lookahead: It asserts that a given pattern can be matched.)
+passwordRegex = re.compile(r'''(
+    ^(?=.{8,15}$)            # Length
+    (?=.*[A-Z])             # Upper
+    (?=.*[a-z])             # Lower
+    (?=.*[0-9])             # Digit
+    (?=.*[!@#$%^&*_-]).*$      # Symbol
+    )''', re.VERBOSE)
+
+password = passwordRegex.search(user_password)
+if password != None:
+    print("Your password is strong")
+else:
+    print("Your password is not secure")
