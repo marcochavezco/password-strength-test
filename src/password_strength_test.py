1c62d3d1d731451d3481abd820d7b8bfe1dfaa6b
marcochavezco <64514382+marcochavezco@users.noreply.github.com>
2021-05-25 18:55:53 -0500

    Update password_strength_test.py

--- src/password_strength_test.py
+++ src/password_strength_test.py
@@ -4,11 +4,11 @@ user_password = str(input("Enter a password: "))
 
 # (?=...) positive lookahead: It asserts that a given pattern can be matched.)
 passwordRegex = re.compile(r'''(
-    ^(?=.{8,15}$)            # Length
+    ^(?=.{8,15}$)           # Length
     (?=.*[A-Z])             # Upper
     (?=.*[a-z])             # Lower
     (?=.*[0-9])             # Digit
-    (?=.*[!@#$%^&*_-]).*$      # Symbol
+    (?=.*[!@#$%^&*_-]).*$   # Symbol
     )''', re.VERBOSE)
 
 password = passwordRegex.search(user_password)
